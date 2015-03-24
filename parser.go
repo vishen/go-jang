12ced3474bfcb449e9c38de99a847f0ab4992016
Jonathan Pentecost <pentecostjonathan@gmail.com>
2015-03-24 18:00:55 +1100

    Possibly _better_ parser + tests

--- parser.go
+++ parser.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 )
 
+const (
+	START_NODE_TAG = "html"
+)
+
 type NodeAttribute struct {
 	name   string
 	values []string
@@ -34,17 +38,15 @@ func WalkNode(root *Node) {
 func Parser(tokens []Token) *Node {
 	var root_node *Node
 	var current_node *Node
-	var previous_node *Node
 	var attribute_values []string
 
-	//all_nodes := []*Node{}
+	nodes := []*Node{}
 
 	var current_nodeattr NodeAttribute
 
 	var current_token Token
 	//var previous_token Token
-
-	max_depth := 0
+	waiting := true
 	current_depth := 0
 
 	pos := 0
@@ -56,49 +58,75 @@ func Parser(tokens []Token) *Node {
 		}
 		//previous_token = current_token
 		current_token = tokens[pos]
-		switch current_token.token_type {
-		case OpenTag:
-			if tokens[pos+1].token_type == ForwardSlash {
-				// Need to close of current node
-				pos += 3
-				current_depth -= 1
-				if current_node.parent != nil {
-					previous_node = current_node.parent
-					previous_node.children = append(previous_node.children, current_node)
-					// fmt.Printf("Adding child %s ###### %s\n", current_node.tag, previous_node.tag)
-					fmt.Printf("%v [%p]\n", previous_node, previous_node)
-					current_node = previous_node
 
-				}
-			} else {
-				current_node = &Node{attributes: []NodeAttribute{}, parent: current_node, children: make([]*Node, 0)}
-				//all_nodes = append(all_nodes, current_node)
-				if root_node == nil {
-					root_node = current_node
-				} else {
-					current_depth += 1
-					max_depth += 1
-				}
+		if waiting {
+			if current_token.token_type == Tag && current_token.value == START_NODE_TAG {
+				waiting = false
+				pos -= 2
 			}
-		case CloseTag:
-			previous_node = current_node
-		case Tag:
-			current_node.tag = current_token.value
-		case Attribute:
-			//fmt.Println(current_token.value)
-			current_nodeattr = NodeAttribute{name: current_token.value, values: []string{}}
-		case Value:
-			attribute_values = strings.Split(current_token.value, " ")
-			current_nodeattr.values = attribute_values
 
-			current_node.attributes = append(current_node.attributes, current_nodeattr)
-		case Text:
-			previous_node.text = current_token.value
+		} else {
 
+			switch current_token.token_type {
+			case OpenTag:
+				if tokens[pos+1].token_type == ForwardSlash {
+					// Need to close of current node
+					// pos += 3
+					// current_depth -= 1
+					// if current_node.parent != nil {
+					// 	previous_node = current_node.parent
+					// 	previous_node.children = append(previous_node.children, current_node)
+					// 	// fmt.Printf("Adding child %s ###### %s\n", current_node.tag, previous_node.tag)
+					// 	fmt.Printf("%v [%p]\n", previous_node, previous_node)
+					// 	current_node = previous_node
+
+					// }
+
+					close_tag := tokens[pos+2].value
+					next_children := []*Node{}
+					i := len(nodes) - 1
+					for ; i > 0; i-- {
+						if nodes[i].tag == close_tag {
+							nodes[i].children = append(nodes[i].children, next_children...)
+							break
+						}
+
+						next_children = append(next_children, nodes[i])
+					}
+					nodes = nodes[0:i]
+					pos += 3
+					current_depth -= 1
+
+				} else {
+					current_node = &Node{attributes: []NodeAttribute{}, children: make([]*Node, 0)}
+					root_node = current_node
+					nodes = append(nodes, current_node)
+				}
+			case CloseTag:
+				// previous_node = current_node
+			case Tag:
+				current_node.tag = current_token.value
+			case Attribute:
+				//fmt.Println(current_token.value)
+				current_nodeattr = NodeAttribute{name: current_token.value, values: []string{}}
+			case Value:
+				attribute_values = strings.Split(current_token.value, " ")
+				current_nodeattr.values = attribute_values
+
+				current_node.attributes = append(current_node.attributes, current_nodeattr)
+			case Text:
+				current_node.text = current_token.value
+
+			}
 		}
 
 		pos += 1
 
 	}
+
+	for i, node := range nodes {
+		fmt.Printf("%d, %s\n", i, node)
+	}
+
 	return root_node
 }
